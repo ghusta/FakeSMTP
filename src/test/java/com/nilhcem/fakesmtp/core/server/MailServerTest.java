9db380359ddeb29656866413df8b6593f6dd7fbe
Guillaume HUSTA <guillaume.husta@gmail.com>
2023-02-27 14:42:07 +0100

    Code cleaning

--- src/test/java/com/nilhcem/fakesmtp/core/server/MailServerTest.java
+++ src/test/java/com/nilhcem/fakesmtp/core/server/MailServerTest.java
@@ -1,19 +1,16 @@
 package com.nilhcem.fakesmtp.core.server;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
-import java.util.Observable;
-import java.util.Observer;
-
-import com.nilhcem.fakesmtp.core.I18n;
 import com.nilhcem.fakesmtp.model.EmailModel;
 import com.nilhcem.fakesmtp.model.UIModel;
 import com.nilhcem.fakesmtp.server.MailSaver;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Observer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,7 +18,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 
 public class MailServerTest {
 	private static MailSaver saver;
@@ -32,12 +28,12 @@ public class MailServerTest {
 	}
 
 	@Test
-	public void testGetLock() {
+	void testGetLock() {
 		assertSame(saver, saver.getLock());
 	}
 
 	@Test
-	public void testSaveDeleteEmail() throws UnsupportedEncodingException, InterruptedException {
+	void testSaveDeleteEmail() {
 		final String from = "from@example.com";
 		final String to = "to@example.com";
 		final String subject = "Hello";
@@ -45,28 +41,25 @@ public class MailServerTest {
 
 		// Save
 		final InputStream data = fromString(getMockEmail(from, to, subject, content));
-		Observer mockObserver = new Observer() {
-			@Override
-			public void update(Observable o, Object arg) {
-				EmailModel model = (EmailModel)arg;
+		Observer mockObserver = (o, arg) -> {
+			EmailModel model = (EmailModel)arg;
 
-				assertEquals(from, model.getFrom());
-				assertEquals(to, model.getTo());
-				assertEquals(subject, model.getSubject());
-				assertEquals(to, model.getTo());
-				assertNotNull(model.getEmailStr());
-				assertFalse(model.getEmailStr().isEmpty());
-				assertNotNull(model.getFilePath());
-				assertFalse(model.getFilePath().isEmpty());
+			assertEquals(from, model.getFrom());
+			assertEquals(to, model.getTo());
+			assertEquals(subject, model.getSubject());
+			assertEquals(to, model.getTo());
+			assertNotNull(model.getEmailStr());
+			assertFalse(model.getEmailStr().isEmpty());
+			assertNotNull(model.getFilePath());
+			assertFalse(model.getFilePath().isEmpty());
 
-				File file = new File(model.getFilePath());
-				assertTrue(file.exists());
+			File file = new File(model.getFilePath());
+			assertTrue(file.exists());
 
-				// Delete
-				UIModel.INSTANCE.getListMailsMap().put(0, model.getFilePath());
-				saver.deleteEmails();
-				assertFalse(file.exists());
-			}
+			// Delete
+			UIModel.INSTANCE.getListMailsMap().put(0, model.getFilePath());
+			saver.deleteEmails();
+			assertFalse(file.exists());
 		};
 		saver.addObserver(mockObserver);
 		assertThat(saver.countObservers()).isNotZero();
@@ -94,7 +87,7 @@ public class MailServerTest {
 		return sb.toString();
 	}
 
-	private InputStream fromString(String str) throws UnsupportedEncodingException {
+	private InputStream fromString(String str) {
 		byte[] bytes = str.getBytes(StandardCharsets.UTF_8);
 		return new ByteArrayInputStream(bytes);
 	}
